6ee7a860f7935eb4f4d38f19fd0173660b7d9a8f
Muhammad Yaqoob <yaqoobmsd2514@gmail.com>
2025-07-18 12:21:07 +0500

    Day 2: Added user input and calculator programs

---
+++ Day02_Inputs_Outputs/calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int y,n;
+    cout<<"Enter Two numbers:";
+    cin>>y>>n;
+    cout<<"\nSum:"<<(y+n)<<endl;
+    cout<<"Difference:"<<(y-n)<<endl;
+    cout<<"Product:"<<(y*n)<<endl;
+    cout<<"Quotient:"<<(y/n)<<endl;
+    return 0;
+}
---
+++ Day02_Inputs_Outputs/user_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    cout <<"Enter Your Name"<< endl;
+    cin>> name;
+    cout <<"Enter Your Age"<< endl;
+    cin>> age;
+    return 0;
+}
