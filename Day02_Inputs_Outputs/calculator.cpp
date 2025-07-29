db89f5bf48d56645f851afc5f78c753039b0ccd1
Muhammad Yaqoob <yaqoobmsd2514@gmail.com>
2025-07-29 15:05:59 +0500

    Day 4: Added loops and pattern programs

--- Day02_Inputs_Outputs/calculator.cpp
+++ Day02_Inputs_Outputs/calculator.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int y,n;
-    cout<<"Enter Two numbers:";
-    cin>>y>>n;
-    cout<<"\nSum:"<<(y+n)<<endl;
-    cout<<"Difference:"<<(y-n)<<endl;
-    cout<<"Product:"<<(y*n)<<endl;
-    cout<<"Quotient:"<<(y/n)<<endl;
+int main()
+{
+    int y, n;
+    cout << "Enter Two numbers:";
+    cin >> y >> n;
+    cout << "\nSum:" << (y + n) << endl;
+    cout << "Difference:" << (y - n) << endl;
+    cout << "Product:" << (y * n) << endl;
+    cout << "Quotient:" << (y / n) << endl;
     return 0;
 }
--- Day02_Inputs_Outputs/user_input.cpp
+++ Day02_Inputs_Outputs/user_input.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     string name;
     int age;
-    cout <<"Enter Your Name"<< endl;
-    cin>> name;
-    cout <<"Enter Your Age"<< endl;
-    cin>> age;
+    cout << "Enter Your Name" << endl;
+    cin >> name;
+    cout << "Enter Your Age" << endl;
+    cin >> age;
     return 0;
 }
